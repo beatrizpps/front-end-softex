0f2c6c82abc883fe4fc48aa06eb1c253990fe83d
Beatriz Pereira <beatrizzz.pps@gmail.com>
2024-07-02 20:25:00 -0300

    string

---
+++ atividade-02-tipo-de-dados/string.ts
@@ -0,0 +1,23 @@
+//Questão 1
+const nome: string = "Beatriz";
+console.log(nome);
+
+//Questão 2
+const frase: string = "Bom dia"
+console.log(frase.length);
+
+//Questão 3
+let palavra: string = "Livro";
+palavra = "L";
+console.log(palavra);
+
+//Questao 4
+const fraseDois: string = "Hoje está calor";
+console.log(fraseDois.toUpperCase());
+
+//Questão 5
+let endereco: string = "Rua da Softex";
+endereco = "Rua da Guia";
+endereco = "Rua da Guia, 142";
+endereco = "Rua da Guia, 142 - Recife";
+console.log(endereco);
